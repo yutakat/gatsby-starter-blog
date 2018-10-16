0a9f467daff8e54d230c1f7f4b1c339ff5bdd9a8
Yutaka Tanida <yutaka@ap.nonsensecorner.com>
2018-10-16 17:33:40 +0900

    Change name again

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,8 +48,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `www.nonsensecorner.com blog`,
+        short_name: `nonsensecorner`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
