6f95ccb8d4450db6cb166b0d1866b1a2285b8faa
Yutaka Tanida <yutaka@ap.nonsensecorner.com>
2018-10-25 22:03:19 +0900

    Refresh bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -13,14 +13,24 @@ class Bio extends React.Component {
       <div
         style={{
           display: 'flex',
-          marginBottom: rhythm(2.5),
+          marginBottom: rhythm(1.0),
         }}
       >
         <p>
-          Written by <strong>L.star</strong> 
-          <a href="https://twitter.com/L_star">
-            follow him on Twitter
-          </a>
+          <dl>
+          <dd> Author: <strong>L.star</strong> </dd>
+          <dd> 
+            Twitter: <a href="https://twitter.com/L_star">
+              @L_star
+            </a>
+          </dd>
+          <dd>
+            Blog: <a href="http://blog.nonsensecorner.com/">
+                blog.nonsensecorner.com
+            </a>
+          </dd>
+
+          </dl>
         </p>
       </div>
     )
